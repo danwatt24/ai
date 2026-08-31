65098578a1aa9541c9c2fdaf9119bdee59e9fa03
Dan Watt <dwatt@incyteenergysolutions.com>
2026-08-30 23:09:23 -0700

    Migrated the session structure and everything related over to a new home inside packages under ltm. This allows the library to be reused by the server or eventual test harnesses/suites

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -2,7 +2,7 @@ import { Hono } from "hono";
 import { serve } from "@hono/node-server";
 import { cors } from "hono/cors";
 import { streamText } from "hono/streaming";
-import { Session } from "./session";
+import { Session } from "@repo/ltm";
 
 const app = new Hono();
 app.use("*", cors());
